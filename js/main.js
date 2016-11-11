75bab3168350712c99a4dcfe40a3f343f815a840
arianadziedzic <arianadziedzic@users.noreply.github.com>
2016-11-11 15:41:34 -0500

    revert to ES2015 for safari compatability

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
+'use strict';
+
 // Hello Script Functionality
-window.onload = function() {
-  let elements = document.getElementsByClassName('txt-rotate');
-  for (let i = 0; i < elements.length; i++) {
-    let toRotate = elements[i].getAttribute('data-rotate');
+window.onload = function () {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
     if (toRotate) {
       new TxtRotate(elements[i], JSON.parse(toRotate), 1000);
     }
   }
 };
 
-let TxtRotate = function(el, toRotate, period) {
+var TxtRotate = function TxtRotate(el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
   this.loopNum = 0;
@@ -19,9 +21,9 @@ let TxtRotate = function(el, toRotate, period) {
   this.isDeleting = false;
 };
 
-TxtRotate.prototype.tick = function() {
-  let i = this.loopNum % this.toRotate.length;
-  let fullTxt = this.toRotate[i];
+TxtRotate.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
 
   if (this.isDeleting) {
     this.txt = fullTxt.substring(0, this.txt.length - 1);
@@ -29,12 +31,14 @@ TxtRotate.prototype.tick = function() {
     this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
 
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
 
-  let that = this;
-  let delta = 300 - Math.random() * 100;
+  var that = this;
+  var delta = 300 - Math.random() * 100;
 
-  if (this.isDeleting) { delta /= 2; }
+  if (this.isDeleting) {
+    delta /= 2;
+  }
 
   if (!this.isDeleting && this.txt === fullTxt) {
     delta = this.period;
@@ -45,42 +49,42 @@ TxtRotate.prototype.tick = function() {
     delta = 500;
   }
 
-  setTimeout(function() {
+  setTimeout(function () {
     that.tick();
   }, delta);
 };
 
 // Modal Functionality
 function modalPortfolio() {
-	el = document.querySelector('#modal-portfolio');
-  otherEl = document.querySelector('#modal-about');
-	el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+  var el = document.getElementById('modal-portfolio');
+  var otherEl = document.getElementById('modal-about');
+  el.style.visibility = el.style.visibility == "visible" ? "hidden" : "visible";
   otherEl.style.visibility = "hidden";
 }
 
 function modalAbout() {
-	el = document.querySelector('#modal-about');
-  otherEl = document.querySelector('#modal-portfolio');
-	el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
-  otherEl.style.visibility = "hidden";
+  var elTwo = document.getElementById('modal-about');
+  var otherElTwo = document.getElementById('modal-portfolio');
+  elTwo.style.visibility = elTwo.style.visibility == "visible" ? "hidden" : "visible";
+  otherElTwo.style.visibility = "hidden";
 }
 
 // Slideshow Functionality
-let slides = document.querySelectorAll('#slides .slide');
-let currentSlide = 0;
-let slideInterval = setInterval(nextSlide, 3000);
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide, 3000);
 
 function slideMovement(n) {
   slides[currentSlide].className = 'slide';
-  currentSlide = (n + slides.length)%slides.length;
+  currentSlide = (n + slides.length) % slides.length;
   slides[currentSlide].className = 'slide showing';
 }
 
 function nextSlide() {
-    slideMovement(currentSlide+1);
+  slideMovement(currentSlide + 1);
 }
 
 function previousSlide() {
-    slideMovement(currentSlide-1);
-    clearInterval(slideInterval);
+  slideMovement(currentSlide - 1);
+  clearInterval(slideInterval);
 }
